824404e55eaa114a7c9a356ffc4912fa2f440e28
Martin Zikmund <martin@zikmund.dev>
2023-05-14 08:53:22 +0200

    Adding layout from original codebase

---
+++ src/web/MZikmund.Web/Helpers/Constants/ViewDataProperties.cs
@@ -0,0 +1,8 @@
+﻿namespace MZikmund.Web.Helpers.Constants;
+
+public class ViewDataProperties
+{
+	public const string Title = nameof(Title);
+
+	public const string ActiveMenuItem = nameof(ActiveMenuItem);
+}
---
+++ src/web/MZikmund.Web/Scripts/ThemeSwitchManager.ts
@@ -0,0 +1,74 @@
+enum RequestedTheme {
+	Dark = "dark",
+	Light = "light",
+	Auto = "auto"
+}
+
+enum DisplayTheme {
+	Dark = "dark",
+	Light = "light"
+}
+
+export class ThemeSwitchManager {
+
+	public static init() {
+		$(() => {
+			ThemeSwitchManager.initTheme();
+		});
+
+		//ensure theme is set immediately to avoid flash of unstyled content
+		ThemeSwitchManager.updateTheme(false);
+	}
+
+	private static determineOsTheme(): DisplayTheme {
+		if (window.matchMedia &&
+			window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			return DisplayTheme.Dark;
+		} else {
+			return DisplayTheme.Light;
+		}
+	}
+
+	private static updateTheme(loaded: boolean) {
+		var requestedTheme: RequestedTheme = <RequestedTheme>localStorage.getItem('ui-theme');
+
+		if (requestedTheme == null) {
+			requestedTheme = RequestedTheme.Auto;
+		}
+
+		if (loaded) {
+			//clear theme selection
+			$("#theme-dropdown a.dropdown-item").removeClass("selected");
+			$("#theme-dropdown a[data-value='" + requestedTheme.toString().toLowerCase() + "'").addClass("selected");
+		}
+
+		var displayTheme: DisplayTheme;
+
+		if (requestedTheme == RequestedTheme.Auto) {
+			displayTheme = ThemeSwitchManager.determineOsTheme();
+		} else {
+			displayTheme = <DisplayTheme><string>requestedTheme;
+		}
+
+		document.documentElement.removeAttribute("data-theme");
+
+		document.documentElement.setAttribute("data-theme", displayTheme.toString().toLowerCase());
+	}
+
+	private static initTheme() {
+
+		$("#theme-dropdown a.dropdown-item").click(function (e) {
+			e.preventDefault();
+			localStorage.setItem('ui-theme', $(this).data('value'));
+			ThemeSwitchManager.updateTheme(true);
+		});
+
+		if (window.matchMedia) {
+			window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", () => {
+				ThemeSwitchManager.updateTheme(true);
+			});
+		}
+
+		ThemeSwitchManager.updateTheme(true);
+	}
+}
