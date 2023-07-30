e550e4765062eea7791328547e335069f58cf117
Martin Zikmund <martin@zikmund.dev>
2023-07-30 12:28:13 +0200

    Add specification/repository pattern

---
+++ src/web/MZikmund.Web.Data/Infrastructure/ISpecification.cs
@@ -0,0 +1,23 @@
+﻿// Based on https://github.com/EdiWang/Moonglade
+
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace MZikmund.Web.Data.Infrastructure;
+
+public interface ISpecification<T>
+{
+	Expression<Func<T, bool>>? Criteria { get; }
+
+	Func<IQueryable<T>, IIncludableQueryable<T, object>>? Include { get; }
+
+	Expression<Func<T, object>>? OrderBy { get; }
+
+	Expression<Func<T, object>>? OrderByDescending { get; }
+
+	int Take { get; }
+
+	int Skip { get; }
+
+	bool IsPagingEnabled { get; }
+}
