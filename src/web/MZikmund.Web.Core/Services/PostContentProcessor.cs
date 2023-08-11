29afa4829e264151cb25a2a2a3a01297be1a14e1
Martin Zikmund <martin@zikmund.dev>
2023-08-11 17:09:23 +0200

    Gist replacer (via requests)

--- src/web/MZikmund.Web.Core/Services/PostContentProcessor.cs
+++ src/web/MZikmund.Web.Core/Services/PostContentProcessor.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
 using System.Text.RegularExpressions;
 using MZikmund.Web.Core.Dtos.Blog;
 using MZikmund.Web.Services;
@@ -12,11 +14,13 @@ public class PostContentProcessor : IPostContentProcessor
 		IMarkdownConverter markdownConverter)
 	{
 		_markdownConverter = markdownConverter;
+		client.DefaultRequestHeaders.Add("User-Agent", "GistReplacer");
 	}
 
 	public Task<string> ProcessAsync(Post post)
 	{
 		var content = TransformWordpressCaption(post.Content);
+		content = ReplaceGistUrlsWithEmbedScripts(content);
 		content = _markdownConverter.ToHtml(content);
 		return Task.FromResult(content);
 	}
@@ -51,5 +55,72 @@ public class PostContentProcessor : IPostContentProcessor
 		transformedContent = Regex.Replace(transformedContent, captionWithoutLinkPattern, TransformWithoutLink);
 
 		return transformedContent;
+	}
+
+	private static readonly HttpClient client = new HttpClient();
+	private const string GistPattern = @"https://gist.github.com/([\w-]+)/([\w-]+)";
+
+	private static async Task<string?> FetchGistContentAsync(string url)
+	{
+		var match = Regex.Match(url, GistPattern);
+		if (!match.Success) return null;
+
+		var gistId = match.Groups[2].Value;
+		var apiUrl = $"https://api.github.com/gists/{gistId}";
+
+		var response = await client.GetAsync(apiUrl);
+		if (!response.IsSuccessStatusCode) return null;
+
+		var jsonResponse = await response.Content.ReadAsStringAsync();
+		var gistData = JsonConvert.DeserializeObject<GistResponse>(jsonResponse);
+
+		var contentBuilder = new System.Text.StringBuilder();
+		foreach (var file in gistData!.Files!.Values)
+		{
+			contentBuilder.Append($"\n```{file!.Language!.ToLower(CultureInfo.InvariantCulture)}\n{file.Content}\n```\n");
+		}
+
+		return contentBuilder.ToString();
+	}
+
+	public class GistFile
+	{
+		public string? Filename { get; set; }
+		public string? Type { get; set; }
+		public string? Language { get; set; }
+		public string? Content { get; set; }
+	}
+
+	public class GistResponse
+	{
+		public Dictionary<string, GistFile>? Files { get; set; }
+	}
+
+	private static async Task<string> ReplaceGistsAsync(string content)
+	{
+		var matches = Regex.Matches(content, GistPattern);
+
+		foreach (Match match in matches)
+		{
+			var gistContent = await FetchGistContentAsync(match.Value);
+			if (gistContent != null)
+			{
+				content = content.Replace(match.Value, gistContent);
+			}
+		}
+
+		return content;
+	}
+
+	public static string ReplaceGistUrlsWithEmbedScripts(string content)
+	{
+		// Pattern to match gist.github.com URLs
+		string pattern = @"https://gist\.github\.com/[\w-]+/[\w-]+";
+
+		// Use Regex to replace each matched URL with its embed script
+		return Regex.Replace(content, pattern, match =>
+		{
+			return $"<script src=\"{match.Value}.js\"></script>";
+		});
 	}
 }
