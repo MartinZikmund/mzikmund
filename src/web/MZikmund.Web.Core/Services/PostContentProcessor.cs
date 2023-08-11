c9f732de22e4ce89e77f113478af21b884c48f3c
Martin Zikmund <martin@zikmund.dev>
2023-08-11 17:18:19 +0200

    Embedding gists via scripts

--- src/web/MZikmund.Web.Core/Services/PostContentProcessor.cs
+++ src/web/MZikmund.Web.Core/Services/PostContentProcessor.cs
@@ -14,7 +14,6 @@ public class PostContentProcessor : IPostContentProcessor
 		IMarkdownConverter markdownConverter)
 	{
 		_markdownConverter = markdownConverter;
-		client.DefaultRequestHeaders.Add("User-Agent", "GistReplacer");
 	}
 
 	public Task<string> ProcessAsync(Post post)
@@ -57,61 +56,6 @@ public class PostContentProcessor : IPostContentProcessor
 		return transformedContent;
 	}
 
-	private static readonly HttpClient client = new HttpClient();
-	private const string GistPattern = @"https://gist.github.com/([\w-]+)/([\w-]+)";
-
-	private static async Task<string?> FetchGistContentAsync(string url)
-	{
-		var match = Regex.Match(url, GistPattern);
-		if (!match.Success) return null;
-
-		var gistId = match.Groups[2].Value;
-		var apiUrl = $"https://api.github.com/gists/{gistId}";
-
-		var response = await client.GetAsync(apiUrl);
-		if (!response.IsSuccessStatusCode) return null;
-
-		var jsonResponse = await response.Content.ReadAsStringAsync();
-		var gistData = JsonConvert.DeserializeObject<GistResponse>(jsonResponse);
-
-		var contentBuilder = new System.Text.StringBuilder();
-		foreach (var file in gistData!.Files!.Values)
-		{
-			contentBuilder.Append($"\n```{file!.Language!.ToLower(CultureInfo.InvariantCulture)}\n{file.Content}\n```\n");
-		}
-
-		return contentBuilder.ToString();
-	}
-
-	public class GistFile
-	{
-		public string? Filename { get; set; }
-		public string? Type { get; set; }
-		public string? Language { get; set; }
-		public string? Content { get; set; }
-	}
-
-	public class GistResponse
-	{
-		public Dictionary<string, GistFile>? Files { get; set; }
-	}
-
-	private static async Task<string> ReplaceGistsAsync(string content)
-	{
-		var matches = Regex.Matches(content, GistPattern);
-
-		foreach (Match match in matches)
-		{
-			var gistContent = await FetchGistContentAsync(match.Value);
-			if (gistContent != null)
-			{
-				content = content.Replace(match.Value, gistContent);
-			}
-		}
-
-		return content;
-	}
-
 	public static string ReplaceGistUrlsWithEmbedScripts(string content)
 	{
 		// Pattern to match gist.github.com URLs
@@ -120,7 +64,7 @@ public class PostContentProcessor : IPostContentProcessor
 		// Use Regex to replace each matched URL with its embed script
 		return Regex.Replace(content, pattern, match =>
 		{
-			return $"<script src=\"{match.Value}.js\"></script>";
+			return $"\n<script src=\"{match.Value}.js\"></script>\n";
 		});
 	}
 }
