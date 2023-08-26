7d5386effef9519948d61cebaf198273b984f239
Martin Zikmund <martin@zikmund.dev>
2023-08-26 16:53:55 +0200

    Add rewrite rule for legacy blog post URLs

--- src/web/MZikmund.Web/Program.cs
+++ src/web/MZikmund.Web/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using AutoMapper;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
@@ -21,7 +22,12 @@ ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-app.UseRewriter(new RewriteOptions().AddRedirectToNonWwwPermanent());
+app.UseRewriter(new RewriteOptions()
+	.AddRedirect(
+		@"blog\.mzikmund\.[a-zA-Z]{2,3}/\d{4}/\d{1,2}/([a-zA-Z0-9\-]+)/?",
+		"https://mzikmund.dev/blog/$1",
+		(int)HttpStatusCode.PermanentRedirect)
+	.AddRedirectToNonWwwPermanent());
 
 // Todo: Improve this
 using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
