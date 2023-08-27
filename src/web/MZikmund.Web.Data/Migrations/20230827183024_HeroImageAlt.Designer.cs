7ab166b2d5bc8c2660609de626aa2774ec080707
Martin Zikmund <martin@zikmund.dev>
2023-08-27 20:30:43 +0200

    Add Hero image alt to post entity

--- src/web/MZikmund.Web.Data/Migrations/20230827183024_HeroImageAlt.Designer.cs
+++ src/web/MZikmund.Web.Data/Migrations/20230827183024_HeroImageAlt.Designer.cs
@@ -4,6 +4,7 @@ using MZikmund.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MZikmund.Web.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230827183024_HeroImageAlt")]
+    partial class HeroImageAlt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
