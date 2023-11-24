58c86b7833647de3727220f80f1907f55217cc35
Martin Zikmund <martin@zikmund.dev>
2023-11-24 12:39:03 +0100

    Basic dialogs, start MSAL

---
+++ src/app/MZikmund/Dialogs/Admin/AddOrUpdateTagDialog.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MZikmund.ViewModels.Admin;
+
+namespace MZikmund.Dialogs.Admin;
+
+public sealed partial class AddOrUpdateTagDialog : AddOrUpdateTagDialogBase
+{
+	public AddOrUpdateTagDialog() => InitializeComponent();
+}
+
+public abstract partial class AddOrUpdateTagDialogBase : DialogBase<AddOrUpdateTagDialogViewModel>
+{
+}
