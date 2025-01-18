8add3e130bcc23307f20fa6d467015a692f52ec3
Martin Zikmund <martin@zikmund.dev>
2025-01-18 13:31:28 +0100

    Start adding support for blobs

---
+++ src/web/MZikmund.Web/Controllers/Admin/FilesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MZikmund.Web.Controllers.Admin;
+
+/// <summary>
+/// Represents operations for generic files.
+/// </summary>
+[ApiController]
+[Authorize]
+[Route("api/v1/files")]
+public class FilesController
+{
+}
