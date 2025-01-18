dc9682286f9e9401abc3904f93c1ca30e1771058
Martin Zikmund <martin@zikmund.dev>
2025-01-18 15:46:48 +0100

    Restructure commands/queries

--- src/web/MZikmund.Web.Core/Blog/CreateCategoryCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-using MediatR;
-using MZikmund.DataContracts.Blog;
-
-namespace MZikmund.Web.Core.Blog;
-
-public record CreateCategoryCommand(Category NewCategory) : IRequest<Category>;
--- src/web/MZikmund.Web.Core/Blog/GetPostByRouteNameQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using MZikmund.DataContracts.Blog;
-
-namespace MZikmund.Web.Core.Blog;
-
-public record GetPostByRouteNameQuery(string RouteName) : IRequest<Post>;
